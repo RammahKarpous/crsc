caf2974dfb72c6395ee9e21d65a7ff672bf15862
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2020-12-21 11:39:25 +0000

    Completed the add event functionality

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -3,29 +3,86 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
+use App\Models\Meet;
+use App\Models\Member;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class EventsController extends Controller
 {
-    public function store()
+    public function create(Meet $meet)
     {
-        Event::create($this->validateData());
+        return view('events.create', ['meet' => $meet]);
+    }
+
+    public function store()
+    {        
+        $this->validateData();
+        Event::create($this->data());
+        return redirect()->back()->with('success', 'An event has been added.');
     }
 
     public function update(Event $event)
     {
-        $event->update($this->validateData());
+        $this->validateData();
+        $event->update($this->data());
+        return redirect()->back()->with('success', 'An event has been updated.');
     }
 
-    public function addSwimmers()
+    public function addSwimmers(Event $event)
     {
+        if ($event->age_range_id == 1) {
         
+            $minAge = 15;
+
+            $maxDate = Carbon::today()->subYears($minAge)->endOfDay();
+
+            $swimmers = Member::where([
+                ['member_type_id', '=', 2],
+                ['gender', '=', $event->gender],
+                ['dob', '>=', $maxDate]
+            ])->get();
+
+            return view('events.add-swimmers', ['swimmers' => $swimmers, 'event' => $event]);
+        } else if ($event->age_range_id == 2) {
+            $minAge = 13;
+            $maxAge = 17;
+
+            $minDate = Carbon::today()->subYears($maxAge);
+            $maxDate = Carbon::today()->subYears($minAge)->endOfDay();
+
+            $swimmers = Member::where([
+                ['member_type_id', '=', 2],
+                ['gender', '=', $event->gender]
+            ])->whereBetween('dob', [$minDate, $maxDate])->get();
+
+            return view('events.add-swimmers', ['swimmers' => $swimmers, 'event' => $event]);
+        }
+    }
+
+    public function data()
+    {
+        $digits = 5;
+
+        return [
+            'meet_id' => request('meet_id'),
+            'age_range_id' => request('age_range_id'),
+            'start_time' => request('start_time'),
+            'end_time' => request('end_time'),
+            'slug' => 'event-' . rand(pow(10, $digits-1), pow(10, $digits)-1),
+            'gender' => request('gender'),
+            'distance' => request('distance'),
+            'stroke' => request('stroke'),
+            'round' => request('round')
+        ]; 
     }
 
     protected function validateData()
     {
         return request()->validate([
-            'age_range' => 'required',
+            'age_range_id' => 'required',
+            'start_time' => 'required|date_format:H:i',
+            'end_time' => 'required|after_or_equal:start_time|date_format:H:i',
             'gender' => 'required',
             'distance' => 'required',
             'stroke' => 'required',
---
+++ resources/views/components/event-form.blade.php
@@ -0,0 +1,86 @@
+<form class="form" action="{{ route($route, $method === 'put' ? $model->id : '') }}" method="post">
+
+    <input type="hidden" name="meet_id" id="meet_id" value="{{ $method === 'put' ? $model->meet_id : $model->id }}">
+
+    <div class="col-5 mt-10">
+        <div class="form__group gcol-1-3">
+            <label class="form__label" for="age_range_id">Age range</label>
+            <select class="form__input form__input--select" name="age_range_id" id="age_range_id">
+                <option {{ $method === 'put' ? '' : 'selected' }} disabled>Please select an age range</option>
+
+                @foreach ($age_ranges as $age)
+                    <option {{ $age->id === $model->age_range_id ? 'selected' : '' }} value="{{ $age->id }}">{{ $age->age_range }}</option>            
+                @endforeach
+            </select>
+            @error('age_range_id') {{ $message }} @enderror
+        </div>
+
+        <div class="form__group">
+            <label class="form__label" for="start_time">Start time</label>
+            <input class="form__input form__input--time" type="time" id="start_time" name="start_time" min="09:00" max="18:00" value="{{ $method === 'put' ? $model->start_time : '' }}">
+            @error('start_time') {{ $message }} @enderror
+        </div>
+
+        <div class="form__group">
+            <label class="form__label" for="end_time">End time</label>
+            <input class="form__input form__input--time" type="time" id="end_time" name="end_time" min="09:00" max="18:00" value="{{ $method === 'put' ? $model->end_time : '' }}">
+            @error('end_time') {{ $message }} @enderror
+        </div>
+    </div>
+    
+    <div class="mt-10">
+        <label>Gender</label>
+    
+        <div class="col-6">
+            <div class="form__group">
+                <input class="form__radio-button" type="radio" name="gender" id="male" value="male">
+                <label class="form__label" for="male">Male</label>
+            </div>
+
+            <div class="form__group">
+                <input class="form__radio-button" type="radio" name="gender" id="female" value="female">
+                <label class="form__label" for="female">Female</label>
+            </div>
+
+            @error('gender') {{ $message }} @enderror
+        </div>
+    </div>
+
+
+
+    <div class="col-3 my-10 mb-20">
+        <div class="form__group">
+            <label class="form__label" for="distance">Distance</label>
+            <input class="form__input form__input--number" type="number" name="distance" id="distance" value="{{ $method === 'put' ? $model->distance : '' }}" min="0" max="{{ $model->pool_length }}"> <small>(max: {{ $model->pool_length }})</small>
+            @error('distance') {{ $message }} @enderror
+        </div>
+        
+        <div class="form__group">
+            <label for="stroke">Stroke</label>
+            <select class="form__input form__input--select" name="stroke" id="stroke">
+                <option value="front-crawl">Freestyle/Front Crawl</option>
+                <option value="backstroke">Backstroke</option>
+                <option value="breaststroke">Breaststroke</option>
+                <option value="butterfly">Butterfly</option>
+                <option value="sidestroke">Sidestroke</option>
+                <option value="elementary-backstroke">Elementary Backstroke</option>
+                <option value="combat-side-stroke">Combat Side Stroke</option>
+                <option value="trudgen">Trudgen</option>
+            </select>
+            @error('stroke') {{ $message }} @enderror
+        </div>
+        
+        <div class="form__group">
+            <label  class="form__label" for="round">Round</label>
+            <input  class="form__input form__input--number" type="number" name="round" id="round" value="{{ $method === 'put' ? date('Y-m-d', strtotime($model->round)) : '' }}">
+            @error('round') {{ $message }} @enderror
+        </div>
+    </div>
+
+    @if ($method === 'put')
+        @method('PATCH')
+    @endif
+
+    @csrf
+    <input type="submit" class="button button--primary" value="{{ $method === 'put' ? 'update' : 'add' }} Member">
+</form>
